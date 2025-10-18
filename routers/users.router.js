e360931fc5a1058cb20558eb162c649bd5fee5b2
Razie Robatjazy <RazieRobatjazy@gmail.com>
2025-10-18 13:22:27 +0330

    bug fix

--- routers/users.router.js
+++ routers/users.router.js
@@ -5,15 +5,15 @@ const auth = require('../functions/auth');
 
 
 router.post('/login', userController.login);
-router.post('/edit_user',auth, userController.edit_user);
-router.post('/change_password',auth, userController.change_password);
-router.post('/edit_device',auth, userController.edit_device);
-router.post('/device_information', auth,userController.device_information);
-router.post('/rtu_information', userController.rtu_information);
+router.post('/edit_user', auth, userController.edit_user);
+router.post('/change_password', auth, userController.change_password);
+router.post('/edit_device', auth, userController.edit_device);
+router.post('/device_information', auth, userController.device_information);
+router.post('/rtu_information', auth, userController.rtu_information);
 //router.post('/device_information', userController.device_information);
 
-router.get('/all_topics',auth, userController.all_topics);
-router.get('/user_information',auth, userController.user_information);
+router.get('/all_topics', auth, userController.all_topics);
+router.get('/user_information', auth, userController.user_information);
 //router.post('/refresh_token', userController.refreshToken); 
 
 // const express = require('express');
